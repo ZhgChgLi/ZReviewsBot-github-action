f77611bb2f9693667301612bcdd8288fadd88478
zhgchgli <zhgchgli@gmail.com>
2021-05-06 22:55:50 +0800

    Update renew fastlane_session

--- renew_FASTLANE_SESSION.rb
+++ renew_FASTLANE_SESSION.rb
@@ -13,7 +13,21 @@ begin
     if config.iOS == nil || config.iOS['appleID'] == nil
         raise "config.iOS['appleID'] parameter not found in #{configPath}"
     end
-    fastlane_session = Spaceship::SpaceauthRunner.new(username: config.iOS['appleID']).run.session_string
+    app = Spaceship::Tunes::login(config.iOS['appleID'], config.iOS['password'])
+    itc_cookie_content = Spaceship::Tunes.client.store_cookie
+
+    cookies = YAML.safe_load(
+      itc_cookie_content,
+      [HTTP::Cookie, Time], # classes allowlist
+      [],                   # symbols allowlist
+      true                  # allow YAML aliases
+    )
+
+    cookies.select! do |cookie|
+      cookie.name.start_with?("myacinfo") || cookie.name == "dqsid" || cookie.name.start_with?("DES")
+    end
+
+    fastlane_session = cookies.to_yaml.gsub("\n", "\\n")
     File.write("#{Pathname.new(configPath).dirname}/FASTLANE_SESSION", fastlane_session, mode: "w+")
 rescue => error
     puts error
